801ab41d694535f33ec0fdbf5f83a3fa4b8af719
Mauricio Abdala <mauricio.abdala@outlook.com>
2023-10-30 02:05:25 -0300

    Atualização_v.1

--- src/schemas/realEstate.schemas.ts
+++ src/schemas/realEstate.schemas.ts
@@ -5,9 +5,9 @@ import { addresseSchema } from "./addresses.schemas";
 
 export const realEstateSchema = z.object({
   id: z.number().positive(),
-  sold: z.boolean().default(() => false),
+  sold: z.boolean().default(false),
   value: z.number().default(() => 0).or(z.string()),
-  size: z.number(),
+  size: z.number().positive(),
   createdAt: z.string(),
   updatedAt: z.string(),
   address: addresseSchema,
